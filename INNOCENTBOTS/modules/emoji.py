dd07f87c679f4dfcf8fdcafd50bfca9d908196cc
𝐈𝐍𝐍𝐎𝐂𝐄𝐍𝐓 𝐁𝐎𝐘 <155868717+INNOCENTBOY2926@users.noreply.github.com>
2024-08-26 16:19:12 +0530

    Update emoji.py

--- INNOCENTBOTS/modules/emoji.py
+++ INNOCENTBOTS/modules/emoji.py
@@ -1,19 +1,10 @@
 import asyncio
 from random import choice
 from telethon import events
-from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
+from config import X1, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
 from INNOCENTBOTS.data import EMOJI
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
-@X2.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
-@X3.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
-@X4.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
-@X5.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
-@X6.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
-@X7.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
-@X8.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
-@X9.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
-@X10.on(events.NewMessage(incoming=True, pattern=r"\%semoji(?: |$)(.*)" % hl))
 async def emoji(e):
      if e.sender_id in SUDO_USERS:
         xraid = e.text.split(" ", 2)

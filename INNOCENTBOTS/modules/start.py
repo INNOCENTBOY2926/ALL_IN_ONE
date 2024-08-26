9b8230c6b9402679cf9a42ee6a292cad9f23ecde
𝐈𝐍𝐍𝐎𝐂𝐄𝐍𝐓 𝐁𝐎𝐘 <155868717+INNOCENTBOY2926@users.noreply.github.com>
2024-08-26 16:24:52 +0530

    Update start.py

--- INNOCENTBOTS/modules/start.py
+++ INNOCENTBOTS/modules/start.py
@@ -1,6 +1,6 @@
 from telethon import __version__, events, Button
 
-from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10
+from config import X1
 
 
 START_BUTTON = [
@@ -16,15 +16,6 @@ START_BUTTON = [
 
 
 @X1.on(events.NewMessage(pattern="/start"))
-@X2.on(events.NewMessage(pattern="/start"))
-@X3.on(events.NewMessage(pattern="/start"))
-@X4.on(events.NewMessage(pattern="/start"))
-@X5.on(events.NewMessage(pattern="/start"))
-@X6.on(events.NewMessage(pattern="/start"))
-@X7.on(events.NewMessage(pattern="/start"))
-@X8.on(events.NewMessage(pattern="/start"))
-@X9.on(events.NewMessage(pattern="/start"))
-@X10.on(events.NewMessage(pattern="/start"))
 async def start(event):
     if event.is_private:
         AltBot = await event.client.get_me()

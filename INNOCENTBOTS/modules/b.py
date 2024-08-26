271e2e911f87feeb034487165fa61b3461488dc1
𝐈𝐍𝐍𝐎𝐂𝐄𝐍𝐓 𝐁𝐎𝐘 <155868717+INNOCENTBOY2926@users.noreply.github.com>
2024-08-26 16:17:53 +0530

    Update b.py

--- INNOCENTBOTS/modules/b.py
+++ INNOCENTBOTS/modules/b.py
@@ -4,7 +4,7 @@ from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights
-from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
+from config import X1, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
 
 # Define ban rights
 RIGHTS = ChatBannedRights(

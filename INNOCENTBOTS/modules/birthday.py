8c495f217b6a1764f6cadedc5e7fb38ec3aa46f6
𝐈𝐍𝐍𝐎𝐂𝐄𝐍𝐓 𝐁𝐎𝐘 <155868717+INNOCENTBOY2926@users.noreply.github.com>
2024-08-26 16:17:39 +0530

    Update birthday.py

--- INNOCENTBOTS/modules/birthday.py
+++ INNOCENTBOTS/modules/birthday.py
@@ -1,7 +1,7 @@
 import asyncio
 from random import choice
 from telethon import events
-from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
+from config import X1, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
 from INNOCENTBOTS.data import BDAY, HRAID, Innotron
 
 
